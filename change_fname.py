081e8f392f2d620c26f09fde071d149b1af93bd3
William Schoenell <wschoenell@gmail.com>
2016-02-25 02:47:58 -0300

    Added HIERARCH T80S DET OUTPUTS to change_fname

--- change_fname.py
+++ change_fname.py
@@ -7,4 +7,5 @@ for fname in sys.argv[1:]:
     print 'Updating %s' % fname
     a = pf.open(fname, 'update')
     a[0].header.set('FILENAME', os.path.basename(fname))
+    a[0].header.set('HIERARCH T80S DET OUTPUTS', 16)
     a.close()
