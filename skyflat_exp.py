b381224ddfb920a353d29684ca424de9092bbf24
William Schoenell <wschoenell@gmail.com>
2016-05-28 18:11:32 -0300

    updated to run in multiple filters

--- skyflat_exp.py
+++ skyflat_exp.py
@@ -5,7 +5,6 @@ import sys
 from astropy.io import fits
 import string
 import matplotlib.pyplot as pl
-from scipy.stats import mode
 from scipy.optimize import curve_fit
 
 
@@ -13,61 +12,77 @@ def expArg(x, a, b, c):
     return a * np.exp(b * x) + c
 
 
-def imlist(dir, filter_name):
+def imlist(dirs, filter_name):
     imglist = []
-    for fname in os.listdir(sys.argv[1]):
-        if fname.endswith('.fits'):
-            fname = os.path.join(sys.argv[1], fname)
-            if fits.getheader(fname)['FILTER'] == filter_name:
-                imglist.append(fname)
+    for dirname in dirs:
+        for fname in os.listdir(dirname):
+            if fname.endswith('.fits'):
+                # try:
+                fname = os.path.join(dirname, fname)
+                header = fits.getheader(fname)
+                if header['FILTER'] == filter_name and header['EXPTIME'] > 0:
+                    imglist.append(fname)
+                    # except:
+                    #     pass
     return imglist
 
 
+colors = ['magenta', 'cyan', 'blue', 'green', 'black', 'red', 'darkmagenta', 'darkcyan', 'darkblue', 'darkgreen',
+          'gray', 'darkred']
+
+coefficients = dict()
+
 pl.clf()
+i_color = 0
+for filter_id in sys.argv[-1].split(','):
+    for dirname in sys.argv[1:-1]:
+        imglist = imlist([dirname], filter_id)
+        sun_alt = np.zeros(len(imglist))
+        count_median = np.zeros(len(imglist))
+        i = 0
+        for file in imglist:
+            image = fits.open(file)
+            long = image[0].header['LONGITUD']
+            lat = image[0].header['LATITUDE']
+            elevation = float(image[0].header['ALTITUDE'])
+            date = image[0].header['DATE-OBS']
+            date = string.replace(date, '-', '/')
+            date = string.replace(date, 'T', ' ')
+            date = string.replace(date, '\'', '')
+            exptime = image[0].header['EXPTIME']
+            observatory = ephem.Observer()
+            observatory.long = long
+            observatory.lat = lat
+            observatory.elevation = elevation
+            observatory.date = date
+            sun = ephem.Sun()
+            sun.compute(observatory)
+            aux_counts = np.average(image[0].data[1000:9000, 1000:9000])
+            if aux_counts < 65000 and image[0].header['IMAGETYP'].startswith(
+                    'sky') and sun.alt > -0.17:  # To remove saturated images
+                count_median[i] = aux_counts / exptime
+                sun_alt[i] = sun.alt
+                print observatory.date, sun.alt, sun.az, exptime, sun_alt[i], count_median[i], file
+                i += 1
+            image.close()
 
-for filter_id in sys.argv[2].split(','):
-    imglist = imlist(sys.argv[1], filter_id)
-    sun_alt = np.zeros(len(imglist))
-    count_median = np.zeros(len(imglist))
-    i = 0
-    for file in imglist:
-        image = fits.open(file)
-        long = image[0].header['LONGITUD']
-        lat = image[0].header['LATITUDE']
-        elevation = float(image[0].header['ALTITUDE'])
-        date = image[0].header['DATE-OBS']
-        date = string.replace(date, '-', '/')
-        date = string.replace(date, 'T', ' ')
-        date = string.replace(date, '\'', '')
-        exptime = image[0].header['EXPTIME']
-        observatory = ephem.Observer()
-        observatory.long = long
-        observatory.lat = lat
-        observatory.elevation = elevation
-        observatory.date = date
-        observatory.date += 4.0 * ephem.hour
-        sun = ephem.Sun()
-        sun.compute(observatory)
-        count_median[i] = np.average(image[0].data[1000:9000, 1000:9000]) / exptime
-        # pl.imshow(image[0].data[1000:9000,1000:9000],cmap='gray')
-        # pl.colorbar()
-        sun_alt[i] = sun.alt
-        print observatory.date, sun.alt, sun.az, exptime, sun_alt[i], count_median[i], file
-        # print i,x[i],y[i]
-        i += 1
-        # pl.plot(image[0].data.sum(axis=0))
-        # pl.show()
-        image.close()
+        sun_alt = sun_alt[count_median > 0]
+        count_median = count_median[count_median > 0]
+        aux_arg = np.argsort(sun_alt)
+        sun_alt = sun_alt[aux_arg]
+        count_median = count_median[aux_arg]
+        print sun_alt
+        print count_median
 
-    print sun_alt
-    print count_median
+        popt, pcov = curve_fit(expArg, sun_alt, count_median, maxfev=100000000, p0=[2000000, 100, 100])
+        print filter_id, popt[0], popt[1], popt[2]
+        coefficients[filter_id] = [popt[0], popt[1], popt[2]]
+        x = np.linspace(np.min(sun_alt), np.max(sun_alt), 100)
+        pl.plot(x * 57.30, expArg(x, popt[0], popt[1], popt[2]), color=colors[i_color])
+        pl.plot(sun_alt * 57.30, count_median, 'o', label=filter_id, color=colors[i_color])
+        pl.xlabel('Sun altitude (deg)')
+        pl.ylabel('counts/sec')
 
-    popt, pcov = curve_fit(expArg, sun_alt, count_median, maxfev=1000000) #, p0=[2000000, 68, 17])
-    print popt[0], popt[1], popt[2]
-    pl.plot(sun_alt, expArg(sun_alt, popt[0], popt[1], popt[2]))
-
-    pl.plot(sun_alt, count_median, label=filter_id)
-    pl.xlabel('Sun altitude')
-    pl.ylabel('Normalized median counts')
+        i_color += 1
 
 pl.legend()
